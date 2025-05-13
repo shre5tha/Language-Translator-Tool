9f8720d9ff7efe11a345cbcc08471e95eab566b4
Shrestha Karmakar <shresthakarmakar221@gmail.com>
2025-05-13 12:56:02 +0530

    ui

---
+++ front-end/language.js
@@ -0,0 +1,17 @@
+const languages = [
+  
+    {
+      no: "16",
+      name: "English",
+      native: "English",
+      code: "en",
+    },
+  
+    {
+      no: "27",
+      name: "Hindi",
+      native: "हिन्दी",
+      code: "hi",
+    },
+  
+  ];
